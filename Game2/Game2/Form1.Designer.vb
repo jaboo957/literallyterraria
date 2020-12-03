62d4758f84375a31bbfc0ec87feb31a1a1ddb2f0
jaboo957 <75336776+jaboo957@users.noreply.github.com>
2020-12-03 13:03:44 -0700

    Add files via upload

---
+++ Game2/Game2/Form1.Designer.vb
@@ -0,0 +1,362 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Terraria
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Arena16WALL = New System.Windows.Forms.PictureBox()
+        Me.Arena10WALL = New System.Windows.Forms.PictureBox()
+        Me.Arena11WALL = New System.Windows.Forms.PictureBox()
+        Me.Arena12WALL = New System.Windows.Forms.PictureBox()
+        Me.Arena13WALL = New System.Windows.Forms.PictureBox()
+        Me.Arena14WALL = New System.Windows.Forms.PictureBox()
+        Me.Arena15WALL = New System.Windows.Forms.PictureBox()
+        Me.Arena2WALL = New System.Windows.Forms.PictureBox()
+        Me.Arena3WALL = New System.Windows.Forms.PictureBox()
+        Me.Arena32WALL = New System.Windows.Forms.PictureBox()
+        Me.Arena4WALL = New System.Windows.Forms.PictureBox()
+        Me.Arena5WALL = New System.Windows.Forms.PictureBox()
+        Me.Arena6WALL = New System.Windows.Forms.PictureBox()
+        Me.Arena7WALL = New System.Windows.Forms.PictureBox()
+        Me.Arena8WALL = New System.Windows.Forms.PictureBox()
+        Me.Arena9WALL = New System.Windows.Forms.PictureBox()
+        Me.SCal = New System.Windows.Forms.PictureBox()
+        Me.ArenaWALL = New System.Windows.Forms.PictureBox()
+        Me.BSDart = New System.Windows.Forms.PictureBox()
+        Me.PC = New System.Windows.Forms.PictureBox()
+        Me.Projectile = New System.Windows.Forms.PictureBox()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.Arena16WALL, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Arena10WALL, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Arena11WALL, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Arena12WALL, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Arena13WALL, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Arena14WALL, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Arena15WALL, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Arena2WALL, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Arena3WALL, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Arena32WALL, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Arena4WALL, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Arena5WALL, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Arena6WALL, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Arena7WALL, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Arena8WALL, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Arena9WALL, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.SCal, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ArenaWALL, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BSDart, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PC, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Projectile, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'Arena16WALL
+        '
+        Me.Arena16WALL.Image = Global.Game2.My.Resources.Resources.Arena_Block__28placed_29
+        Me.Arena16WALL.Location = New System.Drawing.Point(4, 402)
+        Me.Arena16WALL.Name = "Arena16WALL"
+        Me.Arena16WALL.Size = New System.Drawing.Size(53, 50)
+        Me.Arena16WALL.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Arena16WALL.TabIndex = 21
+        Me.Arena16WALL.TabStop = False
+        '
+        'Arena10WALL
+        '
+        Me.Arena10WALL.Image = Global.Game2.My.Resources.Resources.Arena_Block__28placed_29
+        Me.Arena10WALL.Location = New System.Drawing.Point(283, 402)
+        Me.Arena10WALL.Name = "Arena10WALL"
+        Me.Arena10WALL.Size = New System.Drawing.Size(51, 50)
+        Me.Arena10WALL.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Arena10WALL.TabIndex = 20
+        Me.Arena10WALL.TabStop = False
+        '
+        'Arena11WALL
+        '
+        Me.Arena11WALL.Image = Global.Game2.My.Resources.Resources.Arena_Block__28placed_29
+        Me.Arena11WALL.Location = New System.Drawing.Point(236, 402)
+        Me.Arena11WALL.Name = "Arena11WALL"
+        Me.Arena11WALL.Size = New System.Drawing.Size(51, 50)
+        Me.Arena11WALL.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Arena11WALL.TabIndex = 19
+        Me.Arena11WALL.TabStop = False
+        '
+        'Arena12WALL
+        '
+        Me.Arena12WALL.Image = Global.Game2.My.Resources.Resources.Arena_Block__28placed_29
+        Me.Arena12WALL.Location = New System.Drawing.Point(189, 402)
+        Me.Arena12WALL.Name = "Arena12WALL"
+        Me.Arena12WALL.Size = New System.Drawing.Size(51, 50)
+        Me.Arena12WALL.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Arena12WALL.TabIndex = 18
+        Me.Arena12WALL.TabStop = False
+        '
+        'Arena13WALL
+        '
+        Me.Arena13WALL.Image = Global.Game2.My.Resources.Resources.Arena_Block__28placed_29
+        Me.Arena13WALL.Location = New System.Drawing.Point(144, 402)
+        Me.Arena13WALL.Name = "Arena13WALL"
+        Me.Arena13WALL.Size = New System.Drawing.Size(51, 50)
+        Me.Arena13WALL.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Arena13WALL.TabIndex = 17
+        Me.Arena13WALL.TabStop = False
+        '
+        'Arena14WALL
+        '
+        Me.Arena14WALL.Image = Global.Game2.My.Resources.Resources.Arena_Block__28placed_29
+        Me.Arena14WALL.Location = New System.Drawing.Point(100, 402)
+        Me.Arena14WALL.Name = "Arena14WALL"
+        Me.Arena14WALL.Size = New System.Drawing.Size(51, 50)
+        Me.Arena14WALL.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Arena14WALL.TabIndex = 16
+        Me.Arena14WALL.TabStop = False
+        '
+        'Arena15WALL
+        '
+        Me.Arena15WALL.Image = Global.Game2.My.Resources.Resources.Arena_Block__28placed_29
+        Me.Arena15WALL.Location = New System.Drawing.Point(52, 402)
+        Me.Arena15WALL.Name = "Arena15WALL"
+        Me.Arena15WALL.Size = New System.Drawing.Size(51, 50)
+        Me.Arena15WALL.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Arena15WALL.TabIndex = 15
+        Me.Arena15WALL.TabStop = False
+        '
+        'Arena2WALL
+        '
+        Me.Arena2WALL.Image = Global.Game2.My.Resources.Resources.Arena_Block__28placed_29
+        Me.Arena2WALL.Location = New System.Drawing.Point(702, 402)
+        Me.Arena2WALL.Name = "Arena2WALL"
+        Me.Arena2WALL.Size = New System.Drawing.Size(51, 50)
+        Me.Arena2WALL.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Arena2WALL.TabIndex = 14
+        Me.Arena2WALL.TabStop = False
+        '
+        'Arena3WALL
+        '
+        Me.Arena3WALL.Image = Global.Game2.My.Resources.Resources.Arena_Block__28placed_29
+        Me.Arena3WALL.Location = New System.Drawing.Point(654, 402)
+        Me.Arena3WALL.Name = "Arena3WALL"
+        Me.Arena3WALL.Size = New System.Drawing.Size(51, 50)
+        Me.Arena3WALL.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Arena3WALL.TabIndex = 13
+        Me.Arena3WALL.TabStop = False
+        '
+        'Arena32WALL
+        '
+        Me.Arena32WALL.Image = Global.Game2.My.Resources.Resources.Arena_Block__28placed_29
+        Me.Arena32WALL.Location = New System.Drawing.Point(606, 402)
+        Me.Arena32WALL.Name = "Arena32WALL"
+        Me.Arena32WALL.Size = New System.Drawing.Size(51, 50)
+        Me.Arena32WALL.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Arena32WALL.TabIndex = 12
+        Me.Arena32WALL.TabStop = False
+        '
+        'Arena4WALL
+        '
+        Me.Arena4WALL.Image = Global.Game2.My.Resources.Resources.Arena_Block__28placed_29
+        Me.Arena4WALL.Location = New System.Drawing.Point(558, 402)
+        Me.Arena4WALL.Name = "Arena4WALL"
+        Me.Arena4WALL.Size = New System.Drawing.Size(51, 50)
+        Me.Arena4WALL.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Arena4WALL.TabIndex = 11
+        Me.Arena4WALL.TabStop = False
+        '
+        'Arena5WALL
+        '
+        Me.Arena5WALL.Image = Global.Game2.My.Resources.Resources.Arena_Block__28placed_29
+        Me.Arena5WALL.Location = New System.Drawing.Point(512, 402)
+        Me.Arena5WALL.Name = "Arena5WALL"
+        Me.Arena5WALL.Size = New System.Drawing.Size(51, 50)
+        Me.Arena5WALL.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Arena5WALL.TabIndex = 10
+        Me.Arena5WALL.TabStop = False
+        '
+        'Arena6WALL
+        '
+        Me.Arena6WALL.Image = Global.Game2.My.Resources.Resources.Arena_Block__28placed_29
+        Me.Arena6WALL.Location = New System.Drawing.Point(466, 402)
+        Me.Arena6WALL.Name = "Arena6WALL"
+        Me.Arena6WALL.Size = New System.Drawing.Size(51, 50)
+        Me.Arena6WALL.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Arena6WALL.TabIndex = 9
+        Me.Arena6WALL.TabStop = False
+        '
+        'Arena7WALL
+        '
+        Me.Arena7WALL.Image = Global.Game2.My.Resources.Resources.Arena_Block__28placed_29
+        Me.Arena7WALL.Location = New System.Drawing.Point(421, 402)
+        Me.Arena7WALL.Name = "Arena7WALL"
+        Me.Arena7WALL.Size = New System.Drawing.Size(51, 50)
+        Me.Arena7WALL.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Arena7WALL.TabIndex = 8
+        Me.Arena7WALL.TabStop = False
+        '
+        'Arena8WALL
+        '
+        Me.Arena8WALL.Image = Global.Game2.My.Resources.Resources.Arena_Block__28placed_29
+        Me.Arena8WALL.Location = New System.Drawing.Point(375, 402)
+        Me.Arena8WALL.Name = "Arena8WALL"
+        Me.Arena8WALL.Size = New System.Drawing.Size(51, 50)
+        Me.Arena8WALL.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Arena8WALL.TabIndex = 7
+        Me.Arena8WALL.TabStop = False
+        '
+        'Arena9WALL
+        '
+        Me.Arena9WALL.Image = Global.Game2.My.Resources.Resources.Arena_Block__28placed_29
+        Me.Arena9WALL.Location = New System.Drawing.Point(328, 402)
+        Me.Arena9WALL.Name = "Arena9WALL"
+        Me.Arena9WALL.Size = New System.Drawing.Size(51, 50)
+        Me.Arena9WALL.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Arena9WALL.TabIndex = 6
+        Me.Arena9WALL.TabStop = False
+        '
+        'SCal
+        '
+        Me.SCal.Image = Global.Game2.My.Resources.Resources.Scal
+        Me.SCal.Location = New System.Drawing.Point(12, 142)
+        Me.SCal.Name = "SCal"
+        Me.SCal.Size = New System.Drawing.Size(117, 96)
+        Me.SCal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.SCal.TabIndex = 4
+        Me.SCal.TabStop = False
+        '
+        'ArenaWALL
+        '
+        Me.ArenaWALL.Image = Global.Game2.My.Resources.Resources.Arena_Block__28placed_29
+        Me.ArenaWALL.Location = New System.Drawing.Point(749, 402)
+        Me.ArenaWALL.Name = "ArenaWALL"
+        Me.ArenaWALL.Size = New System.Drawing.Size(51, 50)
+        Me.ArenaWALL.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.ArenaWALL.TabIndex = 3
+        Me.ArenaWALL.TabStop = False
+        '
+        'BSDart
+        '
+        Me.BSDart.Image = Global.Game2.My.Resources.Resources.BrimstoneHellblast
+        Me.BSDart.Location = New System.Drawing.Point(144, 178)
+        Me.BSDart.Name = "BSDart"
+        Me.BSDart.Size = New System.Drawing.Size(33, 22)
+        Me.BSDart.TabIndex = 2
+        Me.BSDart.TabStop = False
+        '
+        'PC
+        '
+        Me.PC.Image = Global.Game2.My.Resources.Resources.Auric_Tesla_armor
+        Me.PC.Location = New System.Drawing.Point(517, 327)
+        Me.PC.Name = "PC"
+        Me.PC.Size = New System.Drawing.Size(46, 69)
+        Me.PC.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PC.TabIndex = 1
+        Me.PC.TabStop = False
+        '
+        'Projectile
+        '
+        Me.Projectile.Image = Global.Game2.My.Resources.Resources.BigFlare
+        Me.Projectile.Location = New System.Drawing.Point(461, 346)
+        Me.Projectile.Name = "Projectile"
+        Me.Projectile.Size = New System.Drawing.Size(50, 50)
+        Me.Projectile.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Projectile.TabIndex = 0
+        Me.Projectile.TabStop = False
+        Me.Projectile.Visible = False
+        '
+        'Terraria
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Arena16WALL)
+        Me.Controls.Add(Me.Arena10WALL)
+        Me.Controls.Add(Me.Arena11WALL)
+        Me.Controls.Add(Me.Arena12WALL)
+        Me.Controls.Add(Me.Arena13WALL)
+        Me.Controls.Add(Me.Arena14WALL)
+        Me.Controls.Add(Me.Arena15WALL)
+        Me.Controls.Add(Me.Arena2WALL)
+        Me.Controls.Add(Me.Arena3WALL)
+        Me.Controls.Add(Me.Arena32WALL)
+        Me.Controls.Add(Me.Arena4WALL)
+        Me.Controls.Add(Me.Arena5WALL)
+        Me.Controls.Add(Me.Arena6WALL)
+        Me.Controls.Add(Me.Arena7WALL)
+        Me.Controls.Add(Me.Arena8WALL)
+        Me.Controls.Add(Me.Arena9WALL)
+        Me.Controls.Add(Me.SCal)
+        Me.Controls.Add(Me.ArenaWALL)
+        Me.Controls.Add(Me.BSDart)
+        Me.Controls.Add(Me.PC)
+        Me.Controls.Add(Me.Projectile)
+        Me.Name = "Terraria"
+        Me.Text = "Terraria"
+        CType(Me.Arena16WALL, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Arena10WALL, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Arena11WALL, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Arena12WALL, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Arena13WALL, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Arena14WALL, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Arena15WALL, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Arena2WALL, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Arena3WALL, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Arena32WALL, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Arena4WALL, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Arena5WALL, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Arena6WALL, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Arena7WALL, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Arena8WALL, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Arena9WALL, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.SCal, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ArenaWALL, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BSDart, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PC, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Projectile, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Projectile As PictureBox
+    Friend WithEvents PC As PictureBox
+    Friend WithEvents BSDart As PictureBox
+    Friend WithEvents ArenaWALL As PictureBox
+    Friend WithEvents SCal As PictureBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Arena9WALL As PictureBox
+    Friend WithEvents Arena8WALL As PictureBox
+    Friend WithEvents Arena7WALL As PictureBox
+    Friend WithEvents Arena6WALL As PictureBox
+    Friend WithEvents Arena5WALL As PictureBox
+    Friend WithEvents Arena4WALL As PictureBox
+    Friend WithEvents Arena32WALL As PictureBox
+    Friend WithEvents Arena3WALL As PictureBox
+    Friend WithEvents Arena2WALL As PictureBox
+    Friend WithEvents Arena15WALL As PictureBox
+    Friend WithEvents Arena14WALL As PictureBox
+    Friend WithEvents Arena13WALL As PictureBox
+    Friend WithEvents Arena12WALL As PictureBox
+    Friend WithEvents Arena11WALL As PictureBox
+    Friend WithEvents Arena10WALL As PictureBox
+    Friend WithEvents Arena16WALL As PictureBox
+    Friend WithEvents Timer2 As Timer
+End Class
---
+++ Game2/Game2/Form1.vb
@@ -0,0 +1,172 @@
+﻿Public Class Terraria
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs)
+        SCal.Location = New Point(SCal.Location.X + 5, SCal.Location.Y + 5)
+    End Sub
+    Sub Move1(P As PictureBox, X As Integer, Y As Integer)
+        P.Location = New Point(P.Location.X, P.Location.Y)
+    End Sub
+    Private Sub Timer1_Tick_1(sender As Object, e As EventArgs)
+        follow(SCal)
+        PC.Location = New Point(PC.Location.X, PC.Location.Y)
+    End Sub
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        Select Case e.KeyCode
+            Case Keys.R
+                SCal.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
+                Me.Refresh()
+            Case Keys.Up
+                MoveTo(PC, 0, -5)
+            Case Keys.Down
+                MoveTo(PC, 0, 5)
+            Case Keys.Left
+                MoveTo(PC, -5, 0)
+            Case Keys.Right
+                MoveTo(PC, 5, 0)
+            Case Keys.Space
+                CreateNew("bullet", Projectile, PC.Location)
+
+            Case Else
+                Projectile.Location = PC.Location
+                Projectile.Visible = True
+                Timer1.Enabled = True
+        End Select
+    End Sub
+    Sub CreateNew(name As String, pic As PictureBox, location As Point)
+        Dim p As New PictureBox
+        p.Location = location
+        p.Image = pic.Image
+        p.BackColor = pic.BackColor
+        p.Name = name
+        p.Width = pic.Width
+        p.Height = pic.Height
+        p.SizeMode = PictureBoxSizeMode.StretchImage
+        Controls.Add(p)
+    End Sub
+    Sub follow(p As PictureBox)
+        Static headstart As Integer
+        Static c As New Collection
+        c.Add(SCal.Location)
+        headstart = headstart + 1
+        If headstart > 10 Then
+            p.Location = c.Item(1)
+            c.Remove(1)
+        End If
+    End Sub
+    Public Sub follow(PC)
+        Dim x, y As Integer
+        If PC.Location.X > SCal.Location.X Then
+            x = -5
+        Else
+            x = 5
+        End If
+        MoveTo(PC, x, 0)
+        If PC.Location.Y < SCal.Location.Y Then
+            y = 5
+        Else
+            y = -5
+        End If
+        MoveTo(PC, x, y)
+    End Sub
+    Function Collision(p As PictureBox, t As String)
+        Dim col As Boolean
+
+        For Each c In Controls
+            Dim obj As Control
+            obj = c
+            If p.Bounds.IntersectsWith(obj.Bounds) And (obj.Name.ToUpper.EndsWith(t.ToUpper) Or obj.Name.ToUpper.StartsWith(t.ToUpper)) Then
+                col = True
+            End If
+        Next
+        Return col
+    End Function
+    'Return true or false if moving to the new location is clear of objects ending with t
+    Function IsClear(p As PictureBox, distx As Integer, disty As Integer, t As String) As Boolean
+        Dim b As Boolean
+
+        p.Location += New Point(distx, disty)
+        b = Not Collision(p, t)
+        p.Location -= New Point(distx, disty)
+        Return b
+    End Function
+    'Moves and object (won't move onto objects containing  "wall" and shows green if object ends with "win"
+    Function Collision(p As String, t As String, Optional ByRef other As Object = vbNull)
+        Return Collision(getObject(p), t, other)
+    End Function
+    Sub MoveTo(p As PictureBox, distx As Integer, disty As Integer)
+        If p Is Nothing Then
+            Return
+        End If
+        If IsClear(p, distx, disty, "WALL") Then
+            p.Location += New Point(distx, disty)
+        End If
+        Dim other As Object = Nothing
+        If Collision("BSDart", "PC", other) Then
+            getObject("BSDart").Visible = False
+            getObject("PC").Visible = False
+            Me.BackColor = Color.IndianRed
+        End If
+        If IsClear(p, distx, disty, "WALL") Then
+            p.Location += New Point(distx, disty)
+        End If
+        If Collision("SCal", "Projectile", other) Then
+            getObject("SCal").Visible = False
+            getObject("Projectile").Visible = False
+            other.visible = False
+            Me.BackColor = Color.Green
+            Return
+        End If
+    End Sub
+    Function Collision(p As PictureBox, t As String, Optional ByRef other As Object = vbNull)
+        Dim col As Boolean
+
+        For Each c In Controls
+            Dim obj As Control
+            obj = c
+            If obj.Visible AndAlso p.Bounds.IntersectsWith(obj.Bounds) And obj.Name.ToUpper.Contains(t.ToUpper) Then
+                col = True
+                other = obj
+            End If
+        Next
+        Return col
+    End Function
+    Sub MoveTo(p As String, distx As Integer, disty As Integer)
+        For Each c In Controls
+            If c.name.toupper.ToString.Contains(p.ToUpper) Then
+                MoveTo(c, distx, disty)
+            End If
+        Next
+    End Sub
+    Public Sub Timer2_Tick_1(sender As Object, e As EventArgs)
+        follow(SCal)
+        Static headstart As Integer
+        Static c As New Collection
+        c.Add(PC.Location)
+        headstart = headstart + 1
+        If headstart > 10 Then
+            PC.Location = c.Item(1)
+            c.Remove(1)
+        End If
+    End Sub
+    Public Sub chase(p As PictureBox)
+        Dim x, y As Integer
+        If p.Location.X > PC.Location.X Then
+            x = -5
+        Else
+            x = 5
+        End If
+        MoveTo(p, x, 0)
+        If p.Location.Y < SCal.Location.Y Then
+            y = 5
+        Else
+            y = -5
+        End If
+        MoveTo(p, x, y)
+    End Sub
+    Private Sub Timer1_Tick_2(sender As Object, e As EventArgs) Handles Timer1.Tick
+        MoveTo("bullet", -10, 0)
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        SCal.Image.RotateFlip(RotateFlipType.Rotate90FlipX)
+        PC.Image.RotateFlip(RotateFlipType.Rotate180FlipY)
+    End Sub
+End Class
